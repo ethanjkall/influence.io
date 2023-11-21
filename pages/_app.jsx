fcd8ac485d60511caabce175eb633e39265c8e96
Ethan Kallarackal <ethanjkall@yahoo.com>
2023-11-21 04:04:53 -0500

    Fixed build errors and added env references

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,11 +6,11 @@ import Navbar from '../components/Navbar'
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["latin"] });
 
-export default function App({ Component, pageProps: session , ...pageProps }) {
+export default function App({ Component, pageProps}) {
   // If page layout is available, use it. Else return the page
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <SessionProvider session={session}>
+    <SessionProvider>
       {getLayout(
         <main className={inter.className}>
           <Component {...pageProps} />
